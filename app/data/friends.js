1c63817ea9385b6c70227e57f29f32703b4247eb
Hrynchak <bohdanhrynchak@gmail.com>
2019-06-04 16:18:47 -0500

    friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,24 @@
 var friendMatch = [
   {
-    name: "Aaron Rodgers",
-    photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg ",
+    name: "Jim Carrey",
+    photo: "https://media1.giphy.com/media/l378fIHIADPMXGwww/giphy.gif?cid=790b76115cf6d98b666a346c51efafe1&rid=giphy.gif ",
     scores: ["2", "2", "2", "2", "2"]
   }, {
-    name: "Aaron Judge",
-    photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
+    name: "Donald Trump",
+    photo: "https://media.giphy.com/media/Qjmp5vKEERPyw/giphy.gif",
     scores: ["3", "3", "3", "3", "3"]
   }, {
-    name: " Alex Morgan",
-    photo: "https://i.ytimg.com/vi/LH8RJ9Q0X0M/maxresdefault.jpg ",
+    name: " Sasha Grey",
+    photo: "https://media2.giphy.com/media/g3i9BBPfWppL2/giphy.gif?cid=790b76115cf6d73c77654d6c550fceea&rid=giphy.gif ",
     scores: ["1", "1", "1", "1", "1"]
   }, {
-    name: "Christen Press",
-    photo: "http://cdn.shopify.com/s/files/1/0661/5963/files/0010_christen_press_large.jpg?9601849381797396010 ",
-    scores: ["3", "3", "3", "3", "1"]
+    name: "Dwayne The Rock Johnson",
+    photo: "https://media1.giphy.com/media/QLvRBqfLXCphu/giphy.gif?cid=790b76115cf6db8c37524c3177f76669&rid=giphy.gif",
+    scores: ["3", "2", "3", "1", "1"]
+  },{
+    name: "Eminem",
+    photo: "https://media0.giphy.com/media/wTfk6aw4ffag0/giphy.gif?cid=790b76115cf6ddfd644c306849b22096&rid=giphy.gif",
+    scores: ["2", "2", "1", "3", "3"]
   },
 
 ];
